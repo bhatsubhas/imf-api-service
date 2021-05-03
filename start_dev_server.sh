ad9923e31bd63db3481369e24484aab473832d30
Subhas Bhat <bhatsubhas@gmail.com>
2021-05-03 12:37:22 +0530

    Update startup scripts to activate the venv automatically.

--- start_dev_server.sh
+++ start_dev_server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source venv/bin/activate
+
 export FLASK_APP=imfapi.app
 export FLASK_ENV=development
 flask run
--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+source ./venv/bin/activate
 gunicorn imfapi.app:app
